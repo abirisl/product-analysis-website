ab0bbbb44d1e3f20bad3ad61f06f785b28433536
abir islam <mhabir979292@gmail.com>
2022-04-04 18:10:14 +0600

    Readme file limk set

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useReviews from '../../hooks/customerReviews';
 import img from '../../images/Drone3.png';
-import Reviews from '../Reviews/Reviews';
 import './Home.css'
 
 const Home = () => {
